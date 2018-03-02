693f698f403d9c28599e34c62c75d24bf5cd1144
maha2895 <maha2895@colorado.edu>
2018-03-01 19:40:54 -0700

    my part of lab 6

---
+++ lab6/questions.sql
@@ -0,0 +1,55 @@
+-- {Matthew Hanley}
+-- {Erik Pronk}
+
+-- 1
+SELECT sname FROM store order by sname;
+
+-- 2
+SELECT sname FROM store order by sname limit 3;
+
+-- 6
+select sum(price) as total from store;
+
+
+-- 7
+select count(*) as items from store;
+
+
+-- 8
+select * from course;
+
+
+-- 9
+select sum(count) as total_enrollment
+from enrollment;
+
+
+-- 10
+select count(*) as count
+from course,department
+where department.id=course.department_id;
+
+
+
+-- 11
+select count(department.name) as count from department;
+
+
+-- 12
+select concat(department.name,' ',course.cname) as Classes
+from course,department
+where department.id=course.department_id;
+
+
+-- 13
+SELECT group_concat(department.name,course.cname) as Classes
+from course,department
+where department.id=course.department_id;
+
+
+-- 14
+SELECT * FROM course c
+INNER JOIN enrollment e
+ON e.id=c.id
+INNER JOIN department d
+ON d.id=c.department_id;
