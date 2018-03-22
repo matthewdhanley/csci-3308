ff0ce14aa95035dd1eef5f272230cdc50b4ef4af
matthewhanely <32152060+matthewhanely@users.noreply.github.com>
2018-03-22 17:36:58 -0600

    finished

--- lab9/routes/store.js
+++ lab9/routes/store.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.get('/', function(req, res, next) {
     req.getConnection(function(error, conn) {
-        var sqlQuery = 'select * from store'/*Fill the select query here*/;
+        var sqlQuery = 'select * from store';
 // Query to get all the entries.
 // conn object which will execute and return results
         conn.query(sqlQuery,function(err, rows, fields) {
@@ -102,47 +102,142 @@ app.post('/add', function(req, res, next){
     }
 })
 
-// // PART 3 =======================================================================
-// // SHOW EDIT ITEM FORM - Display form for update
-// app.get('/edit/(:id)', function(req, res, next){
-//     /*
-//     TODO : Update operation is similar to add operation.
-//     Fill out the appropriate code below
-//     Hints :
-//     * req.params.id will give you the id which is passed in
-//     /edit/(:id).Use
-//     that in SQL update query and any other place where you need
-//     id
-//     * You are only displaying an item here for the customer.
-//     * The actual update happens in the post action below this
-//     module
-//     */
-//     req.getConnection(function(error, conn) {
-//         conn.query('UPDATE store SET ? where id like ?', function(err, rows, fields) {
-// // if item not found
-//             if (rows.length <= 0) {
-//                 req.flash('error', 'Item not found with id = ' + req.params.id)
-//                 res.redirect('/store')
-//             }
-//             else { // if item found
-// // render to views/store/edit.ejs template file
-//                 res.render(/*path to template*/, {
-//                     title: 'Edit Item',
-//                     /* Place the code for sending values */
-//                     /* Hint : rows[0] gets the entire tuple
-//                     from database.
-//                     Get the right values from rows[0]
-//                     */
-//                     id: req.params.id,
-//                     qty: req.params.qty,
-//                     price : req.params.price,
-//                     sname : req.params.sname
-//                 })
-//             }
-//         })
-//     })
-// })
+// PART 3 =======================================================================
+// SHOW EDIT ITEM FORM - Display form for update
+app.get('/edit/(:id)', function(req, res, next){
+    /*
+    TODO : Update operation is similar to add operation.
+    Fill out the appropriate code below
+    Hints :
+    * req.params.id will give you the id which is passed in
+    /edit/(:id).Use
+    that in SQL update query and any other place where you need
+    id
+    * You are only displaying an item here for the customer.
+    * The actual update happens in the post action below this
+    module
+    */
+    req.getConnection(function(error, conn) {
+        conn.query('select * from store where id like ?', req.params.id, function(err, rows, fields) {
+// if item not found
+            if (rows.length <= 0) {
+                req.flash('error', 'Item not found with id = ' + req.params.id)
+                res.redirect('/store')
+            }
+            else { // if item found
+// render to views/store/edit.ejs template file
+                res.render('store/edit', {
+                    title: 'Edit Item',
+                    /* Place the code for sending values */
+                    /* Hint : rows[0] gets the entire tuple
+                    from database.
+                    Get the right values from rows[0]
+                    */
+                    id: req.params.id,
+                    qty: req.params.qty,
+                    price : req.params.price,
+                    sname : req.params.sname
+                })
+            }
+        })
+    })
+})
 
+// EDIT ITEM POST ACTION - Update the item, actual update happens here
+app.put('/edit/(:id)', function(req, res, next) {
+    req.assert('sname', 'Name is required').notEmpty()
+//Validate name
+    req.assert('qty', 'Quantity is required').notEmpty()
+//Validate qty
+    req.assert('price', 'Price is required').notEmpty()
+//Validate price
+    var errors = req.validationErrors()
+    if( !errors ) { //No errors were found. Passed Validation!
+        var item = {
+            sname: req.sanitize('sname').escape().trim(),
+            qty: req.sanitize('qty').escape().trim(),
+            price: req.sanitize('price').escape().trim()
+        }
+        req.getConnection(function(error, conn) {
+            conn.query('UPDATE store SET ? where id like ?', [item, req.params.id], function(err, result) {
+                if (err) {
+                    req.flash('error', err)
+// render to views/store/edit.ejs
+                    res.render('store/edit', {
+                        title: 'Edit Item',
+                        id: req.params.id,
+                        /* Since this is update query part,
+                        no rows are returned
+                        You need to fetch below values from
+                        req.body
+                        I have already fetched the item id
+                        above for you.
+                        Beware I have used req.params but
+                        below you need to use req.body*/
+                        sname: req.body.sname,
+                        qty: req.body.qty,
+                        price: req.body.price
+                    })
+                } else {
+                    req.flash('success', 'Data update successfully!')
+// render to views/store/edit.ejs
+                    res.render('store/edit', {
+                        title: 'Edit Item',
+                        id: req.params.id,
+                        /* Since this is update query part,
+                        no rows are returned
+                        You need to fetch below values from
+                        req.body
+                        I have already fetched the item id
+                        above for you.
+                        Beware I have used req.params but
+                        below you need to use req.body*/
+                        sname: req.body.sname,
+                        qty: req.body.qty,
+                        price: req.body.price
+                    })
+                }
+            })
+        })
+    }
+    else { //Display errors to user
+        var error_msg = ''
+        errors.forEach(function(error) {
+            error_msg += error.msg + '<br>'
+        })
+        req.flash('error', error_msg)
+// render to views/store/edit.ejs
+        res.render('store/edit', {
+            title: 'Edit Item,',
+            id: req.params.id,
+/* Since this is update query part, no rows are returned
+You need to fetch below values from req.body
+I have already fetched the item id above for you.
+Beware I have used req.params but below you need to
+use req.body*/
+            sname: req.body.sname,
+            qty: req.body.qty,
+            price: req.body.price
+        })
+    }
+})
+
+// DELETE Item. Pass the right id in the URL.
+app.delete('/delete/(:id)', function(req, res, next) {
+    req.getConnection(function(error, conn) {
+        conn.query('DELETE FROM store where id like ?',req.params.id, function(err, result) {
+            if (err) {
+                req.flash('error', err)
+// redirect to users list page
+                res.redirect('/store')
+            } else {
+                req.flash('success', 'ID deleted. ID: '+req.params.id)
+// redirect to users list page
+                res.redirect('/store')
+            }
+        })
+    })
+})
 
 
 module.exports = app // This must be the last line in the file. Any and all code
