460d6af9b7b12ed4c977ba22396611f960ef663c
ErikJP <erik.pronk@colorado.edu>
2018-04-11 12:19:34 -0600

    hw 4 stuff

---
+++ hw4/cgi-bin/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Erik Pronk and Matthew Hanley 
+
+print "Content-Type: text/html"
+print
+
+contents = """<html>
+			      <head>
+			          <title>
+					      HW #4 Erik Pronk and Matthew Hanley
+					  </title>
+				  </head>
+				  <body>
+					  <object type="image/svg+xml" data="Blank_US_Map_(states_only).svg">
+						  Does not support SVG
+					  </object>
+				  </body>
+			  </html>"""
+
+print contents
