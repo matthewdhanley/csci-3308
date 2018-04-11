31fe8032ca41824ebab9520c49be4926e7be380f
ErikJP <erik.pronk@colorado.edu>
2018-04-11 14:27:49 -0600

    updating up to number 5 in part 2

--- hw4/cgi-bin/index.py
+++ hw4/cgi-bin/index.py
@@ -5,6 +5,59 @@
 import urllib2
 import json
 
+states_caps = {
+        'AL':{'state':'Alabama',        'capital':'Montgomery'},
+        'AK':{'state':'Alaska',         'capital':'Juneau'},
+        'AZ':{'state':'Arizona',        'capital':'Phoenix'},
+        'AR':{'state':'Arkansas',       'capital':'Little Rock'},
+        'CA':{'state':'California',     'capital':'Sacramento'},
+        'CO':{'state':'Colorado',       'capital':'Denver'},
+        'CT':{'state':'Connecticut',    'capital':'Hartford'},
+        'DE':{'state':'Deleware',       'capital':'Dover'},
+        'FL':{'state':'Florida',        'capital':'Tallahassee'},
+        'GA':{'state':'Georgia',        'capital':'Atlanta'},
+        'HI':{'state':'Hawaii',         'capital':'Honolulu'},
+        'ID':{'state':'Idaho',          'capital':'Boise'},
+        'IL':{'state':'Illinois',       'capital':'Springfield'},
+        'IN':{'state':'Indiana',        'capital':'Indianapolis'},
+        'IA':{'state':'Iowa',           'capital':'Des Moines'},
+        'KS':{'state':'Kansas',         'capital':'Topeka'},
+        'KY':{'state':'Kentucky',       'capital':'Frankfort'},
+        'LA':{'state':'Louisiana',      'capital':'Baton Rouge'},
+        'ME':{'state':'Maine',          'capital':'Augusta'},
+        'MD':{'state':'Maryland',       'capital':'Annapolis'},
+        'MA':{'state':'Massachusetts',  'capital':'Boston'},
+        'MI':{'state':'Michigan',       'capital':'Lansing'},
+        'MN':{'state':'Minnesota',      'capital':'Saint Paul'},
+        'MS':{'state':'Mississippi',    'capital':'Jackson'},
+        'MO':{'state':'Missouri',       'capital':'Jefferson City'},
+        'MT':{'state':'Montana',        'capital':'Helena'},
+        'NE':{'state':'Nebraska',       'capital':'Lincoln'},
+        'NV':{'state':'Nevada',         'capital':'Carson City'},
+        'NH':{'state':'New Hampshire',  'capital':'Concord'},
+        'NJ':{'state':'New Jersey',     'capital':'Trenton'},
+        'NM':{'state':'New Mexico',     'capital':'Santa Fe'},
+        'NY':{'state':'New York',       'capital':'Albany'},
+        'NC':{'state':'North Carolina', 'capital':'Raleigh'},
+        'ND':{'state':'North Dakota',   'capital':'Bismarck'},
+        'OH':{'state':'Ohio',           'capital':'Columbus'},
+        'OK':{'state':'Oklahoma',       'capital':'Oklahoma City'},
+        'OR':{'state':'Oregon',         'capital':'Salem'},
+        'PA':{'state':'Pennsylvania',   'capital':'Harrisburg'},
+        'RI':{'state':'Rhode Island',   'capital':'Providence'},
+        'SC':{'state':'South Carolina', 'capital':'Columbia'},
+        'SD':{'state':'South Dakota',   'capital':'Pierre'},
+        'TN':{'state':'Tennessee',      'capital':'Nashville'},
+        'TX':{'state':'Texas',          'capital':'Austin'},
+        'UT':{'state':'Utah',           'capital':'Salt Lake City'},
+        'VT':{'state':'Vermont',        'capital':'Montpelier'},
+        'VA':{'state':'Virginia',       'capital':'Richmond'},
+        'WA':{'state':'Washington',     'capital':'Olympia'},
+        'WV':{'state':'West Virginia',  'capital':'Charleston'},
+        'WI':{'state':'Wisconsin',      'capital':'Madison'},
+        'WY':{'state':'Wyoming',        'capital':'Cheyenne'},
+}
+
 print "Content-Type: text/html"
 print
 
@@ -92,12 +145,15 @@ contents = """<html>
 
 print contents
 
-jsonData = urllib2.urlopen('https://api.forecast.io/forecast/<Insert API key>/40.014984,-105.270546')
+jsonData = urllib2.urlopen('https://api.forecast.io/forecast/63a232e03b63d68f2ee7fec62b36cec4/40.014984,-105.270546')
 data = json.load(jsonData)   
 print jsonData
 print """
 <script>
-$(document).ready(function()){"""
+	$(document).ready(function(){"""
+for key in states_caps.keys():
+	print "$('#%s').css('fill','red')" % (key)
 print """
 });
 </script>"""
+
